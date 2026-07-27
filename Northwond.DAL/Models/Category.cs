cb9b2f8c96b4eea6c4dd059ab0488d5768e4a635
rowidAAA <rowida.abdelghany05@eng-st.cu.edu.eg>
2026-07-27 10:54:46 +0300

    category , customers, employess models in dal

---
+++ Northwond.DAL/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwond.DAL.Models
+{
+    public class Category
+    {
+        [Key]
+        public int categoryID { get; set; }
+        public string categotyName { get; set; }
+        public string description { get; set; }
+        public byte[] picture { get; set; }
+    }
+}
---
+++ Northwond.DAL/Models/Employees.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwond.DAL.Models
+{
+    public class Employees
+    {
+        [Key]
+        public int employeeID {get; set;}
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string title { get; set; }
+        public string titleOfCourtesy { get; set; }
+        public DateTime birthDate { get; set; }
+        public DateTime hireDate { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public int postalcode { get; set; }
+        public string country { get; set; }
+        public int homePhone { get; set; }
+        public int extension { get; set; }
+        public byte[] photo { get; set; }
+        public string notes { get; set; }
+        public string reportsTo { get; set; }
+        public string photoPath { get; set; }
+    }
+}
