abe703eb79d6fee0d52fed0c1feefcf8680c5ea7
rowidAAA <rowida.abdelghany05@eng-st.cu.edu.eg>
2026-08-10 10:52:52 +0300

    controller

---
+++ Northwind.Web/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Web.Mvc;
+using Northwind.BLL;
+using Northwind.DAL.Models;
+
+namespace Northwind.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private OrderService orderService = new OrderService();
+        private CustomerService customerService = new CustomerService();
+        //private EmployeeService employeeService = new EmployeeService();
+        //private ShipperService shipperService = new ShipperService();
+
+        public ActionResult Index()
+        {
+            return View(orderService.GetAll());
+        }
+
+        public ActionResult Create()
+        {
+            LoadDropdowns();
+            return View(new Orders { OrderDate = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ActionResult Create(Orders order)
+        {
+            if (!ModelState.IsValid)
+            {
+                LoadDropdowns();
+                return View(order);
+            }
+
+            try
+            {
+                var saved = orderService.Create(order);
+                return RedirectToAction("Details", new { id = saved.OrderID });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Could not save order: " + ex.Message);
+                LoadDropdowns();
+                return View(order);
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            var order = orderService.GetOrderByID(id);
+            if (order == null) return HttpNotFound();
+            return View(order);
+        }
+
+        private void LoadDropdowns()
+        {
+            ViewBag.CustomerID = new SelectList(customerService.GetAllCustomers(), "CustomerID", "CompanyName");
+            //ViewBag.EmployeeID = new SelectList(employeeService.GetAll(), "EmployeeID", "LastName");
+            //ViewBag.ShipVia = new SelectList(shipperService.GetAll(), "ShipperID", "CompanyName");
+        }
+    }
+}
---
+++ Northwind.Web/Controllers/OrderDetailsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web.Mvc;
+using Northwind.BLL;
+using Northwind.DAL.Models;
+
+namespace Northwind.Web.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        private OrderDetailService detailService = new OrderDetailService();
+        private ProductService productService = new ProductService();
+
+        public ActionResult Create(int orderId)
+        {
+            ViewBag.Products = productService.GetAllProducts();
+            return PartialView(new OrderDetails { OrderID = orderId, Quantity = 1 });
+        }
+
+        [HttpPost]
+        public ActionResult Create(OrderDetails detail)
+        {
+            if (detailService.Exists(detail.OrderID, detail.ProductID))
+            {
+                ModelState.AddModelError("ProductID", "This product is already on the order.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = 400;
+                ViewBag.Products = productService.GetAllProducts();
+                return PartialView(detail);
+            }
+
+            try
+            {
+                detailService.Create(detail);
+                return new HttpStatusCodeResult(200);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Could not save line item: " + ex.Message);
+                Response.StatusCode = 400;
+                ViewBag.Products = productService.GetAllProducts();
+                return PartialView(detail);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int orderId, int productId)
+        {
+            detailService.Delete(orderId, productId);
+            return new HttpStatusCodeResult(200);
+        }
+    }
+}
