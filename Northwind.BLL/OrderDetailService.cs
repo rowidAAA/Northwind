7587757cff1ba3944cb4691d10c52a63c13f9a02
rowidAAA <rowida.abdelghany05@eng-st.cu.edu.eg>
2026-08-10 10:37:04 +0300

    order and order details services done

---
+++ Northwind.BLL/OrderDetailService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Northwind.DAL;
+using Northwind.DAL.Models;
+
+namespace Northwind.BLL
+{
+    public class OrderDetailService
+    {
+        public List<OrderDetails> GetByOrderId(int orderId)
+        {
+            using (var db = new NorthwindContext())
+            {
+                return db.OrderDetails
+                    .Include(od => od.Product)
+                    .Where(od => od.OrderID == orderId)
+                    .ToList();
+            }
+        }
+
+        public bool Exists(int orderId, int productId)
+        {
+            using (var db = new NorthwindContext())
+            {
+                return db.OrderDetails.Any(od => od.OrderID == orderId && od.ProductID == productId);
+            }
+        }
+
+        public void Create(OrderDetails detail)
+        {
+            using (var db = new NorthwindContext())
+            {
+                db.OrderDetails.Add(detail);
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(int orderId, int productId)
+        {
+            using (var db = new NorthwindContext())
+            {
+                var detail = db.OrderDetails.Find(orderId, productId);
+                if (detail != null)
+                {
+                    db.OrderDetails.Remove(detail);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- Northwind.BLL/OrderService.cs
+++ Northwind.BLL/OrderService.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Northwind.DAL.Models;
+using Northwind.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Northwind.BLL
 {
     public class OrderService
     {
+        public List<Orders> GetAll()
+        {
+            using (var db = new NorthwindContext())
+            {
+                return db.Orders.Include(o => o.Customer).OrderByDescending(o => o.OrderID).ToList();
+            }
+        }
+
+        public Orders GetOrderByID(int id)
+        {
+            using (var db =new NorthwindContext())
+            {
+                return db.Orders.Include(o=>o.Customer).Include(o => o.OrderDetails.Select(od => od.Product)).FirstOrDefault(o => o.OrderID == id);
+            }
+        }
+
+        public Orders Create(Orders order)
+        {
+            using (var db = new NorthwindContext())
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return order;
+            }
+        }
     }
 }
